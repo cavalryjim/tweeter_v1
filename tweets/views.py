fc767f53f5b3737283bd11c7ecb290c1e611b515
James Davis <james.davisphd@gmail.com>
2020-03-12 09:40:52 -0500

    Update views.py

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-# tweets/views.py
+# tweets/views.py.  eafd33f7-08b3-4726-99c0-92b7abe3b5f0
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from django.urls import reverse
